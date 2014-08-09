b7e79fa30fe0045bd861a2532a4f196a09027332
Fernanda Bernardo <fernandabernardo94@gmail.com>
2014-08-09 12:22:09 -0300

    melhorando primeiro exemplo

--- EP2/PrimeiroExemplo.c
+++ EP2/PrimeiroExemplo.c
@@ -4,15 +4,29 @@
 int main(int argc, char *argv[]) {
 	printf("\n Olá 1 - Fora da Região Paralela \n\n");
 
+	#pragma omp parallel num_threads(4)
+	{
+		int id = omp_get_thread_num();
+		int nt = omp_get_num_threads();
+		printf("Sou a thread %d de um total %d\n", id, nt);
+	}
+	
+	printf("\n Olá 2 - Fora da Região Paralela \n\n");
+
 	#pragma omp parallel
 	{
 		int id = omp_get_thread_num();
 		int nt = omp_get_num_threads();
 		printf("Sou a thread %d de um total %d\n", id, nt);
 	}
-	printf("\n Olá 2 - Fora da Região Paralela \n\n");
+	printf("\n Olá 3 - Fora da Região Paralela \n\n");
 	return 0;
 }
 
 //usar 'export OMP_NUM_THREADS=X' onde X é o número 
 //de threads que você quer usar
+
+//o num_threads(4) serve para não ter que fazer o export :)
+
+//compilar: 
+//'gcc PrimeiroExemplo.c -o PrimeiroExemplo -fopenmp'
