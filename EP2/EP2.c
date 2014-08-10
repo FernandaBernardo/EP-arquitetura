f9b45f92006b617573e99ed3bbb1b7246b34059c
Renan Freitas <emailinexistente@blah.com>
2014-08-09 22:08:09 -0300

    Expansão dinâmica de array de leitura do arquivo

--- EP2/EP2.c
+++ EP2/EP2.c
@@ -1,41 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <omp.h>
 
-int* abre_arquivo ( char* nome_arquivo ) {
-	FILE *entrada = fopen( nome_arquivo, "r" );
-	
-	fprintf(stdout, "Here\n");
-	if ( ! entrada ) {
-		fprintf(stderr, "ERROR: Arquivo não encontrado!\n");
-		exit(0);
-	};
+#define false 0
+#define true 1
 
-	// printf("\n");
+//Variáveis globais
+size_t int_array_size = 0;
 
-	int aux = 0;
-	
-	int* array = malloc(sizeof(int)*10);
-	
-	while ( ! feof( entrada ) ) {
-		fscanf(entrada, "%d", &array[aux]);
-		// printf("%d ", array[aux]);
-		aux++;
+/* Imprime de 10 em 10 elementos do array */
+void imprime_array(int* array, size_t size) {
+	int i;
+	for (i = 0; i < size; i++) {
+		if( i!=0 && i%10 == 0 ){
+			printf("\n");
+		}
+		printf("%d ", array[i]);
 	}
-
-	// printf("\n\n");
-
-	fclose(entrada);
-
-	return array;
+	printf("\n-----\n");
 }
 
-void imprime_array(int A[], int tam) {
-	int i;
-	for (i = 0; i < tam; i++) {
-		printf("%d ", A[i]);
+/* Expande original, um array de int, do tamanho original para um novo tamanho, que deve ser maior que o original */
+int* expand_array( int* original, size_t original_size, size_t new_size ){
+	if( original_size > new_size ){
+		fprintf( stderr, "ERROR: Argumento invalido. O novo tamanho do array deve ser maior que o antigo.\n");
+		exit( EXIT_FAILURE );
 	}
-	printf("\n");
+
+	printf("expanding array from %d to %d\n", (int)original_size, (int)new_size);
+
+	int* expanded_array;
+
+	if( original_size > (new_size - original_size) ){
+		
+		expanded_array = (int*)realloc( original, new_size);
+		
+		if( expanded_array == NULL ){
+			fprintf( stderr, "ERROR: Memoria insuficiente para alocar array expandido.\n" );
+			exit( EXIT_FAILURE );
+		}
+
+		int index;
+		for( index = new_size - original_size; (index < new_size); index++){
+			expanded_array[index] = 0;
+		}		
+	
+	} else {
+
+		expanded_array = (int*)calloc(new_size, sizeof(int));
+	
+		if( ! expanded_array ){
+			fprintf( stderr, "ERROR: Memoria insuficiente para alocar array expandido.\n" );
+			exit( EXIT_FAILURE );
+		}
+
+		int index;
+		for( index = 0; index < original_size; index++){
+			expanded_array[index] = original[index];
+		}	
+	}
+
+	return expanded_array;
+}
+
+int* le_array_inteiros( char*  nome_arquivo, size_t* size ) {
+
+	FILE *entrada = fopen( nome_arquivo, "rb" );
+	
+	if ( entrada == NULL ) {
+		fprintf(stderr, "ERROR: Arquivo não encontrado!\n");
+		exit(EXIT_FAILURE);
+	};
+
+	rewind( entrada );
+
+	int index = 0;
+	size_t array_size = 10;
+	int *array = (int*)malloc(sizeof(int)*array_size);
+	
+	if( array == NULL){
+		int size_to_print = array_size;
+		fprintf(stderr, "ERROR: Memoria insuficiente para alocar array de %d inteiros. \n", size_to_print);
+		exit(EXIT_FAILURE);
+	}
+
+	while( ! feof( entrada ) ){
+
+
+		int aux = 0;
+		if(  fscanf( entrada, "%d", &aux ) < 1 ){
+			break;
+		}
+		
+		if( index == array_size ){
+			size_t original_size = array_size;
+			array_size = array_size * 2;
+		
+			int* expanded_array = expand_array( array, original_size, array_size);
+			free(array);
+			array = expanded_array;
+		}
+
+		array[index] = aux;
+		index++;
+	}
+
+	fclose(entrada);
+	
+	(*size) = index+1;
+	return array;
 }
 
 void insertion_sequencial (int A[], int tam) {
@@ -84,25 +158,24 @@ void quick_sequencial (int A[], int esquerdo, int direito) {
 }
 
 int main(int argc, char *argv[]) {
-	int* array;
-	printf("argv[1] = %s\n", argv[1]);
-	
-	if( argv[1] ) array = abre_arquivo(argv[1]);
-	else return 0;
+
+	if( argc <= 1 ) {
+		exit(EXIT_FAILURE);
+	} 
+	size_t size;
+	char* file_name = argv[1];
+	int* array = le_array_inteiros( file_name, &size );
 
 	printf("Insertion Sort:\n");
-	int* a = array;
-	int tam = sizeof(a);
-	printf("%d\n", tam);
-	imprime_array(a, tam);
-	insertion_sequencial(a, tam);
-	imprime_array(a, tam);
+	imprime_array(array, size);
+	insertion_sequencial(array, size );
+	imprime_array(array, size);
 
+	// int* array2 = le_array_inteiros( file_name, &size );
 	printf("Quick Sort:\n");
-	int b[] = {4,2,1,3,5,10,7,9,8,6};
-	imprime_array(b, tam);
-	quick_sequencial(b, 0, tam-1);
-	imprime_array(b, sizeof(b)/sizeof(int));
+	imprime_array(array, size);
+	// quick_sequencial(array, 0, size - 1);
+	// imprime_array(array, size);
 
-	return 0;
+	exit( EXIT_SUCCESS );
 }
